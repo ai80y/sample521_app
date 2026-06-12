cb4cddc1b0e22c14f7821d047736cd79f1ac7abe
ai80y <a25101ya@aiit.ac.jp>
2026-06-12 11:17:10 +0000

    Add GET route for logout to fix 404 error

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
+  get    "/logout",  to: "sessions#destroy"
   resources :users do
     member do
       get :following, :followers
